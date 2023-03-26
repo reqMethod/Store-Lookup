9d8ddb5194651960c9fb696bb3d4df04c0d760fa
reqMethod <aboenig9799@gmail.com>
2023-03-26 12:27:02 -0400

    init

---
+++ ProofOfConcept/gui.py
@@ -0,0 +1,13 @@
+import main
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.geometry("790x720")
+
+frame1 = Frame(root)
+frame1.place(x=40, y=10)
+frame2 = Frame(root)
+frame2.place(x=10, y=10)
+
+root.mainloop()
---
+++ ProofOfConcept/main.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+def connect():
+    global conn, c
+    conn = sqlite3.connect("sqliteclass.db")
+    c = conn.cursor()
+
+def create_table():
+    connect()
+    c.execute("""CREATE TABLE IF NOT EXISTS contacts (
+    storenum text, 
+    address text,
+    number text
+    ) """)
+    conn.commit()
+    conn.close()
+
+def add_one(storenum, address, number):
+    connect()
+    c.execute(f"SELECT* FROM contacts WHERE storenum = '{storenum}' AND address = '{address}' AND number = '{number}'")
+    result = c.fetchall
+    if len(result) == 0 and storenum != "" and address != "" and number != "":
+        c.execute("INSERT INTO contacts VALUES (?, ?, ?),", storenum, address, number)
+        conn.commit()
+        conn.close()
+
+def show():
+    connect()
+    c.execute("SELECT rowid, * FROM contacts")
+    result = c.fetchall()
+    conn.commit()
+    conn.close()
+    return result
+
+def delete(id):
+    connect()
+    c.execute(f"DELETE FROM contacts WHERE rowid = {id}")
+    conn.commit()
+    conn.close()
+
+def update(id, storenum="", address="", number=""):
+    connect()
+    if storenum != "":
+        c.execute(f"UPDATE contacts SET storenum='{storenum}' WHERE rowid = {id}")
+    if address != "":
+        c.execute(f"UPDATE contacts SET storenum='{address}' WHERE rowid = {id}")
+    if number != "":
+        c.execute(f"UPDATE contacts SET storenum='{number}' WHERE rowid = {id}")
+    conn.commit
+    conn.close
+
+    
+
+create_table()
